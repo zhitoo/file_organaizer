87f91c6a2446da3ae4c5dbdc132c372d26153630
Hossein Shafiei <hshafiei374@gmail.com>
2024-10-09 15:34:18 +0330

    fix warning

--- main.go
+++ main.go
@@ -42,26 +42,26 @@ func getCurrentDir() (string, error) {
 func MoveFile(sourcePath, destPath string) error {
 	inputFile, err := os.Open(sourcePath)
 	if err != nil {
-		return fmt.Errorf("Couldn't open source file: %v", err)
+		return fmt.Errorf("couldn't open source file: %v", err)
 	}
 	defer inputFile.Close()
 
 	outputFile, err := os.Create(destPath)
 	if err != nil {
-		return fmt.Errorf("Couldn't open dest file: %v", err)
+		return fmt.Errorf("couldn't open dest file: %v", err)
 	}
 	defer outputFile.Close()
 
 	_, err = io.Copy(outputFile, inputFile)
 	if err != nil {
-		return fmt.Errorf("Couldn't copy to dest from source: %v", err)
+		return fmt.Errorf("couldn't copy to dest from source: %v", err)
 	}
 
 	inputFile.Close() // for Windows, close before trying to remove: https://stackoverflow.com/a/64943554/246801
 
 	err = os.Remove(sourcePath)
 	if err != nil {
-		return fmt.Errorf("Couldn't remove source file: %v", err)
+		return fmt.Errorf("couldn't remove source file: %v", err)
 	}
 	return nil
 }
