188c486bad6e3a42c5898ce4144faa2a565d608d
Hossein Shafiei <hshafiei374@gmail.com>
2024-10-09 15:32:19 +0330

    organaize files

--- main.go
+++ main.go
@@ -1,7 +1,88 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func createDirectory(path string) error {
+	// Check if the directory exists
+	_, err := os.Stat(path)
+	if err == nil {
+		return nil
+	}
+	// If the directory does not exist, create its parent
+	if os.IsNotExist(err) {
+		err = createDirectory(filepath.Dir(path))
+		if err != nil {
+			return err
+		}
+		// Create the directory
+		err = os.Mkdir(path, 0777)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func getCurrentDir() (string, error) {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		return "", err
+	}
+
+	return dir, nil
+}
+
+func MoveFile(sourcePath, destPath string) error {
+	inputFile, err := os.Open(sourcePath)
+	if err != nil {
+		return fmt.Errorf("Couldn't open source file: %v", err)
+	}
+	defer inputFile.Close()
+
+	outputFile, err := os.Create(destPath)
+	if err != nil {
+		return fmt.Errorf("Couldn't open dest file: %v", err)
+	}
+	defer outputFile.Close()
+
+	_, err = io.Copy(outputFile, inputFile)
+	if err != nil {
+		return fmt.Errorf("Couldn't copy to dest from source: %v", err)
+	}
+
+	inputFile.Close() // for Windows, close before trying to remove: https://stackoverflow.com/a/64943554/246801
+
+	err = os.Remove(sourcePath)
+	if err != nil {
+		return fmt.Errorf("Couldn't remove source file: %v", err)
+	}
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello!")
+
+	pwd, err := getCurrentDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	files, err := os.ReadDir(pwd)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name(), file.IsDir())
+		ext := filepath.Ext(file.Name())
+		ext = strings.TrimLeft(ext, ".")
+		createDirectory(pwd + "/" + ext)
+		MoveFile(pwd+"/"+file.Name(), pwd+"/"+ext+"/"+file.Name())
+	}
 }
